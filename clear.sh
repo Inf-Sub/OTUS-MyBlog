17310dd086ec51a353914f9062c37bb02d766cc1
InfSub <inf.sub+github.com@gmail.com>
2023-12-18 00:45:35 +0300

    test docker, test web pgudmin4 v5.7

--- clear.sh
+++ clear.sh
@@ -2,7 +2,7 @@
 
 docker compose down --remove-orphans
 docker volume rm otus-myblog_db-data
-docker volume prune -y
+docker volume prune
 # docker volume rm otus-myblog_pgadmin-data
 # docker volume rm otus-myblog_adminer-data
 
